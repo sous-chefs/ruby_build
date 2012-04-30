42784a34d69a1225aa68062ed827f09dc9f61a6d
Fletcher Nichol <fnichol@nichol.ca>
2012-04-30 14:21:49 -0600

    JRuby requires make package on Ubuntu/Debian.

--- attributes/default.rb
+++ attributes/default.rb
@@ -44,7 +44,7 @@ when "debian", "ubuntu"
         zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0
         libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf
         libc6-dev ssl-cert subversion }
-  node.set['ruby_build']['install_pkgs_jruby'] = %w{ g++ }
+  node.set['ruby_build']['install_pkgs_jruby'] = %w{ make g++ }
 
 when "suse"
   node.set['ruby_build']['install_pkgs'] = %w{ tar bash curl git-core }
