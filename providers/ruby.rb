aa41e5abb87a852d77aec242f781b4cbd8e01e99
Fletcher Nichol <fnichol@nichol.ca>
2012-05-03 15:57:15 -0600

    ruby_build_ruby LWRP now notifies when updated (FC017).

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -59,6 +59,7 @@ def perform_install
 
     Chef::Log.debug("ruby_build_ruby[#{@rubie}] build time was " +
       "#{(Time.now - install_start)/60.0} minutes")
+    new_resource.updated_by_last_action(true)
   end
 end
 
