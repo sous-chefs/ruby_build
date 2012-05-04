70ce9ee4ef9e6d1d2b90f5f94f3223d1c927f59e
Fletcher Nichol <fnichol@nichol.ca>
2012-05-03 20:41:26 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Manages the ruby-build framework and its installed rubies. A LWRP is also defined."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.2"
+version          "0.6.3"
