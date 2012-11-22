d3cf8b38c7bcf407ba25b09ee65c13e40b1c5c4e
Fletcher Nichol <fnichol@nichol.ca>
2012-11-21 22:57:10 -0700

    Reformat metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              "ruby_build"
-maintainer       "Fletcher Nichol"
-maintainer_email "fnichol@nichol.ca"
-license          "Apache 2.0"
-description      "Manages the ruby-build framework and its installed rubies. A LWRP is also defined."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.3"
+maintainer        "Fletcher Nichol"
+maintainer_email  "fnichol@nichol.ca"
+license           "Apache 2.0"
+description       "Manages the ruby-build framework and its installed rubies. A LWRP is also defined."
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.6.3"
