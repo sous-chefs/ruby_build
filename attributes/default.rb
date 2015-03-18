702b4ea6b7905a07ac9f8d44485dda505afae614
Chris Lundquist <rampantdurandal@gmail.com>
2015-03-17 22:38:15 -0700

    add libffi-dev for cruby deb packages
    See https://github.com/sstephenson/ruby-build/issues/690#issuecomment-68113987

--- attributes/default.rb
+++ attributes/default.rb
@@ -48,7 +48,7 @@ when "debian", "ubuntu"
     %w{ build-essential bison openssl libreadline6 libreadline6-dev
         zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0
         libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf
-        libc6-dev ssl-cert subversion }
+        libc6-dev ssl-cert subversion libffi-dev }
   node.set['ruby_build']['install_pkgs_jruby'] = %w{ make g++ }
 
 when "suse"
