cc35df0c088675f69bd4b1adc52f9d29bc963481
Dane Hammer <dane.hammer@cerner.com>
2015-08-21 22:22:25 -0500

    hang on to what's working

--- test/cookbooks/alltherubies/recipes/default.rb
+++ test/cookbooks/alltherubies/recipes/default.rb
@@ -18,8 +18,8 @@
 #
 
 cores         = node['cpu']['total'].to_i
-system_rubies = %w{ 1.9.2-p320 1.9.3-p362 2.0.0-p0 2.1.0
-                    jruby-1.7.1 rbx-2.1.1 }
+system_rubies = %w{ rbx-2.1.1 } #%w{ 1.9.2-p320 1.9.3-p362 2.0.0-p0 2.1.0
+                #    jruby-1.7.1 rbx-2.1.1 }
 
 include_recipe "java"
 
