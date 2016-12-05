332f1f042981019eb342e180a912d08f778b6e51
Austin Heiman <atheimanksu@gmail.com>
2016-12-04 18:17:46 -0600

    add chefspec custom resource matchers

---
+++ libraries/matchers.rb
@@ -0,0 +1,10 @@
+if defined?(ChefSpec)
+  ChefSpec.define_matcher(:ruby_build_ruby)
+
+  def install_ruby_build_ruby(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ruby_build_ruby, :install, name)
+  end
+  def reinstall_ruby_build_ruby(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ruby_build_ruby, :reinstall, name)
+  end
+end
