b1fa524c3075ced3cc94af744c798159754945da
Dan Webb <dan.webb@damacus.io>
2017-07-28 09:49:16 +0100

    Fix delivery warnings (#57)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'ruby_build'
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Manages the ruby-build framework and its installed rubies. A LWRP is also defined.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.1.0'
