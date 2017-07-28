f8e4b1e0704c57ac1daa494413f9ef96e14c5f3d
Dan Webb <dan.webb@damacus.io>
2017-07-28 12:53:02 +0100

    Revert to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,56 +29,56 @@ default['ruby_build']['default_ruby_base_path'] = '/usr/local/ruby'
 # ruby-build upgrade action
 default['ruby_build']['upgrade'] = 'none'
 
-# case node['platform_family']
-# when 'rhel', 'fedora'
-#   default['ruby_build']['install_pkgs'] = %w( tar bash curl )
-#   default['ruby_build']['install_pkgs_cruby'] =
-#     %w( gcc-c++ patch readline-devel zlib-devel
-#         libffi-devel openssl-devel libyaml-devel
-#         make bzip2 autoconf automake libtool bison
-#         libxml2 libxml2-devel libxslt libxslt-devel
-#         gdbm-devel )
-#   default['ruby_build']['install_pkgs_rbx'] =
-#     %w( ncurses-devel llvm-static llvm-devel ) + node['ruby_build']['install_pkgs_cruby']
-#   default['ruby_build']['install_pkgs_jruby'] = []
-#
-# when 'debian'
-#   default['ruby_build']['install_pkgs'] = %w( tar bash curl )
-#   default['ruby_build']['install_pkgs_cruby'] =
-#     %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
-#         zlib1g-dev libsqlite3-dev libxml2-dev libxslt1-dev
-#         libc6-dev libffi-dev libgdbm3 libgdbm-dev )
-#   default['ruby_build']['install_pkgs_rbx'] =
-#     %w( libncurses5-dev llvm-3.4 llvm-3.4-dev libeditline-dev libedit-dev
-#       ) + node['ruby_build']['install_pkgs_cruby']
-#
-#   default['ruby_build']['install_pkgs_jruby'] = %w( make g++ )
-#
-# when 'suse'
-#   default['ruby_build']['install_pkgs'] = %w( tar bash curl )
-#   default['ruby_build']['install_pkgs_cruby'] =
-#     %w( gcc-c++ automake patch zlib-devel libffi48-devel libyaml-devel
-#         sqlite3-devel libxml2-devel libxslt-devel autoconf gdbm-devel
-#         libopenssl-devel readline-devel libyaml-devel )
-#   default['ruby_build']['install_pkgs_rbx'] =
-#     %w( ncurses-devel ) + node['ruby_build']['install_pkgs_cruby']
-#   default['ruby_build']['install_pkgs_jruby'] = []
-#
-# when 'mac_os_x'
-#   default['ruby_build']['install_pkgs'] = []
-#   default['ruby_build']['install_pkgs_cruby'] = %w( openssl libyaml libffi )
-#   default['ruby_build']['install_pkgs_jruby'] = []
-#
-# when 'freebsd'
-#   default['ruby_build']['install_pkgs'] = []
-#   default['ruby_build']['install_pkgs_cruby'] =
-#     %w( autoconf autoconf-wrapper automake automake-wrapper indexinfo
-#         libedit libffi libyaml m4 perl5 gmake )
-#   default['ruby_build']['install_pkgs_jruby'] =
-#     %w( alsa-lib bash dejavu expat fixesproto fontconfig freetype2
-#         gettext-runtime giflib indexinfo inputproto java-zoneinfo
-#         javavmwrapper kbproto libICE libSM libX11 libXau libXdmcp libXext
-#         libXfixes libXi libXrender libXt libXtst libfontenc libpthread-stubs
-#         libxcb libxml2 mkfontdir mkfontscale openjdk8 recordproto renderproto
-#         xextproto xproto )
-# end
+case node['platform_family']
+when 'rhel', 'fedora'
+  default['ruby_build']['install_pkgs'] = %w( tar bash curl )
+  default['ruby_build']['install_pkgs_cruby'] =
+    %w( gcc-c++ patch readline-devel zlib-devel
+        libffi-devel openssl-devel libyaml-devel
+        make bzip2 autoconf automake libtool bison
+        libxml2 libxml2-devel libxslt libxslt-devel
+        gdbm-devel )
+  default['ruby_build']['install_pkgs_rbx'] =
+    %w( ncurses-devel llvm-static llvm-devel ) + node['ruby_build']['install_pkgs_cruby']
+  default['ruby_build']['install_pkgs_jruby'] = []
+
+when 'debian'
+  default['ruby_build']['install_pkgs'] = %w( tar bash curl )
+  default['ruby_build']['install_pkgs_cruby'] =
+    %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
+        zlib1g-dev libsqlite3-dev libxml2-dev libxslt1-dev
+        libc6-dev libffi-dev libgdbm3 libgdbm-dev )
+  default['ruby_build']['install_pkgs_rbx'] =
+    %w( libncurses5-dev llvm-3.4 llvm-3.4-dev libeditline-dev libedit-dev
+      ) + node['ruby_build']['install_pkgs_cruby']
+
+  default['ruby_build']['install_pkgs_jruby'] = %w( make g++ )
+
+when 'suse'
+  default['ruby_build']['install_pkgs'] = %w( tar bash curl )
+  default['ruby_build']['install_pkgs_cruby'] =
+    %w( gcc-c++ automake patch zlib-devel libffi48-devel libyaml-devel
+        sqlite3-devel libxml2-devel libxslt-devel autoconf gdbm-devel
+        libopenssl-devel readline-devel libyaml-devel )
+  default['ruby_build']['install_pkgs_rbx'] =
+    %w( ncurses-devel ) + node['ruby_build']['install_pkgs_cruby']
+  default['ruby_build']['install_pkgs_jruby'] = []
+
+when 'mac_os_x'
+  default['ruby_build']['install_pkgs'] = []
+  default['ruby_build']['install_pkgs_cruby'] = %w( openssl libyaml libffi )
+  default['ruby_build']['install_pkgs_jruby'] = []
+
+when 'freebsd'
+  default['ruby_build']['install_pkgs'] = []
+  default['ruby_build']['install_pkgs_cruby'] =
+    %w( autoconf autoconf-wrapper automake automake-wrapper indexinfo
+        libedit libffi libyaml m4 perl5 gmake )
+  default['ruby_build']['install_pkgs_jruby'] =
+    %w( alsa-lib bash dejavu expat fixesproto fontconfig freetype2
+        gettext-runtime giflib indexinfo inputproto java-zoneinfo
+        javavmwrapper kbproto libICE libSM libX11 libXau libXdmcp libXext
+        libXfixes libXi libXrender libXt libXtst libfontenc libpthread-stubs
+        libxcb libxml2 mkfontdir mkfontscale openjdk8 recordproto renderproto
+        xextproto xproto )
+end
--- resources/ruby.rb
+++ resources/ruby.rb
@@ -3,8 +3,10 @@
 # Resource:: ruby
 #
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
+# Author:: Dan Webb <dan.webb@damacus.io>
 #
 # Copyright:: 2011-2016, Fletcher Nichol
+# Copyright:: 2017, Dan Webb
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,8 +24,86 @@
 actions :install, :reinstall
 default_action :install
 
-attribute :definition,  kind_of: String, name_attribute: true
-attribute :prefix_path, kind_of: String
-attribute :user,        kind_of: String
-attribute :group,       kind_of: String
-attribute :environment, kind_of: Hash
+property :definition, String, name_attribute: true
+property :prefix_path, String
+property :user, String
+property :group, String
+property :environment, Hash
+
+action :install do
+  installed = perform_install
+end
+
+action :reinstall do
+  installed = perform_install
+end
+
+action_class do
+  include Chef::Rbenv::PackageDeps
+
+  def root_path
+    new_resource.prefix_path ||
+                    "#{node['ruby_build']['default_ruby_base_path']}/#{new_resource.definition}"
+  end
+
+  def perform_install
+    if ruby_installed?
+      Chef::Log.debug(
+        "ruby_build_ruby[#{new_resource.definition}] is already installed, so skipping")
+      false
+    else
+      install_start = Time.now
+
+      install_ruby_dependencies
+
+      Chef::Log.info(
+        "Building ruby_build_ruby[#{new_resource.definition}], this could take a while...")
+
+      rubie       = new_resource.definition
+      execute "ruby-build[#{rubie}]" do
+        command   %(/usr/local/bin/ruby-build "#{rubie}" "#{root_path}")
+        user        new_resource.user         if new_resource.user
+        group       new_resource.group        if new_resource.group
+        environment new_resource.environment  if new_resource.environment
+
+        action :nothing
+      end.run_action(:run)
+
+      Chef::Log.info("ruby_build_ruby[#{new_resource.definition}] build time was " \
+        "#{(Time.now - install_start) / 60.0} minutes")
+      true
+    end
+  end
+
+  def ruby_installed?
+    if Array(new_resource.action).include?(:reinstall)
+      false
+    else
+      ::File.exist?("#{root_path}/bin/ruby")
+    end
+  end
+
+  def install_ruby_dependencies
+    case ::File.basename(new_resource.definition)
+    when /^\d\.\d\.\d/, /^ree-/
+      pkgs = node['ruby_build']['install_pkgs_cruby']
+    when /^rbx-/
+      pkgs = node['ruby_build']['install_pkgs_rbx']
+    when /^jruby-/
+      pkgs = node['ruby_build']['install_pkgs_jruby']
+    end
+
+    # use multi-package when available since it's much faster
+    if platform_family?('rhel', 'suse', 'debian', 'fedora', 'amazon')
+      package pkgs do
+        action :nothing
+      end.run_action(:install)
+    else
+      Array(pkgs).each do |pkg|
+        package pkg do
+          action :nothing
+        end.run_action(:install)
+      end
+    end
+  end
+end
