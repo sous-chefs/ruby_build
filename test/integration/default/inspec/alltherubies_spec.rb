79216ab4fb5aab51304bbd542f5309ce9cb2b959
Austin Heiman <atheimanksu@gmail.com>
2017-09-21 11:46:34 -0500

    moved bats tests to inspec

--- test/integration/default/inspec/alltherubies_spec.rb
+++ test/integration/default/inspec/alltherubies_spec.rb
@@ -1,8 +1,25 @@
-rubies = %w( 2.2.6 2.3.3 2.4.0 jruby-9.1.7.0 )
+def nokogiri_install_cmd(ruby_dir)
+  "#{ruby_dir}/bin/gem install nokogiri --no-ri --no-rdoc"
+end
 
-rubies.each do |rubie|
-  describe directory("/usr/local/ruby/#{rubie}") do
+def nokogiri_ssl_cmd(ruby_dir)
+  %{ #{ruby_dir}/bin/ruby -rrubygems -ropen-uri -rnokogiri \\
+  -e "puts Nokogiri::HTML(open('https://google.com')).css('input')" }
+end
+
+%w[2.2.6 2.3.3 2.4.0 jruby-9.1.7.0].each do |rubie|
+  ruby_dir = File.join('/usr/local/ruby', rubie)
+
+  describe directory(ruby_dir) do
     it { should be_directory }
+  end
+
+  describe command(nokogiri_install_cmd(ruby_dir)) do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command(nokogiri_ssl_cmd(ruby_dir)) do
+    its('exit_status') { should eq 0 }
   end
 end
 
