adb6044e094262e94654e51fbab0d3787e62c999
Tim Smith <tsmith@chef.io>
2018-05-08 16:36:17 -0700

    Convert to a custom resource
    Resolve foodcritic warnings where we were managing state ourselves.
    Remove the incorrect load current resource usage to save variables off
    and do default values.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -17,12 +17,12 @@
 # limitations under the License.
 #
 
-apt_update 'update'
+apt_update
 
 include_recipe 'java'
 include_recipe 'ruby_build'
 
-%w(2.4.2 jruby-9.1.13.0).each do |rubie|
+%w(2.4.4 jruby-9.1.13.0).each do |rubie|
   ruby_build_ruby rubie do
     environment('MAKE_OPTS' => "-j #{node['cpu']['total'].to_i + 1}")
   end
