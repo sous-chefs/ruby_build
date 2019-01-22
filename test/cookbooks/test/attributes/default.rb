82787456994508d1660170ea82bb83aadd8ec521
Dan Webb <dan.webb@damacus.io>
2019-01-22 21:27:25 +0000

    JDK 7 for debian 7

--- test/cookbooks/test/attributes/default.rb
+++ test/cookbooks/test/attributes/default.rb
@@ -1,1 +1,7 @@
-default['java']['jdk_version'] = '8'
+
+
+if node['platform_family'] == 'debian' && node['platform_version'] == '8'
+  default['java']['jdk_version'] = '7'
+else
+  default['java']['jdk_version'] = '8'
+end
