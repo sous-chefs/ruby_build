da0b7fe347e8a6736ac1762fb662b85f263a66df
Dan Webb <dan.webb@damacus.io>
2019-01-23 18:23:55 +0000

    Only test with JRuby for now

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -4,6 +4,6 @@ ruby_build_install '' do
   jruby_deps true
 end
 
-ruby_build_definition '2.6.0'
+# ruby_build_definition '2.6.0'
 
 ruby_build_definition 'jruby-9.1.13.0'
--- test/integration/default/inspec/rubies_spec.rb
+++ test/integration/default/inspec/rubies_spec.rb
@@ -8,20 +8,18 @@ def ssl_cmd(ruby_dir)
   %{ #{ruby_dir}/bin/ruby -r net/http -e 'Net::HTTP.get(URI("https://letsencrypt.org"))' }
 end
 
-%w(2.6.0 jruby-9.1.13.0).each do |rubie|
-  ruby_dir = File.join('/usr/local/ruby', rubie)
+ruby_dir = File.join('/usr/local/ruby', 'jruby-9.1.13.0')
 
-  describe directory(ruby_dir) do
-    it { should be_directory }
-  end
+describe directory(ruby_dir) do
+  it { should be_directory }
+end
 
-  describe command(gem_install_cmd(ruby_dir)) do
-    its('exit_status') { should eq 0 }
-  end
+describe command(gem_install_cmd(ruby_dir)) do
+  its('exit_status') { should eq 0 }
+end
 
-  describe command(ssl_cmd(ruby_dir)) do
-    its('exit_status') { should eq 0 }
-  end
+describe command(ssl_cmd(ruby_dir)) do
+  its('exit_status') { should eq 0 }
 end
 
 describe file('/usr/local/bin/ruby-build') do
