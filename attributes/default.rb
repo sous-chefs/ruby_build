9e0abdd28cbb90cb7113b01874dd641af60a9301
Jason Field <jason@avon-lea.co.uk>
2019-08-12 08:50:43 +0100

    Package fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -45,7 +45,11 @@ when 'rhel', 'fedora', 'amazon'
 when 'debian'
   default['ruby_build']['install_pkgs'] = %w( tar bash curl )
   default['ruby_build']['install_pkgs_cruby'] =
-    if node['platform_version'].to_i >= 10
+    if node['platform'] == 'ubuntu' && node['platform_version'] == '16.04'
+      %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
+        zlib1g-dev libsqlite3-dev libxml2-dev libxslt1-dev
+        libc6-dev libffi-dev libgdbm3 libgdbm-dev )
+    elsif node['platform_version'].to_i >= 10
       %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
           zlib1g-dev libsqlite3-dev libxml2-dev libxslt1-dev
           libc6-dev libffi-dev libgdbm5 libgdbm-dev )
