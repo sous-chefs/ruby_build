84b5f1d794f0ccf574c587c6a9456a4a590de0e1
Tim Smith <tsmith@chef.io>
2019-10-24 18:34:21 -0700

    Cookstyle fixes (#95)
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -45,7 +45,7 @@ when 'rhel', 'fedora', 'amazon'
 when 'debian'
   default['ruby_build']['install_pkgs'] = %w( tar bash curl )
   default['ruby_build']['install_pkgs_cruby'] =
-    if node['platform'] == 'ubuntu' && node['platform_version'] == '16.04'
+    if platform?('ubuntu') && node['platform_version'] == '16.04'
       %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
           zlib1g-dev libsqlite3-dev libxml2-dev libxslt1-dev
           libc6-dev libffi-dev libgdbm3 libgdbm-dev )
