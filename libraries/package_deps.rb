d44e8567dafb3a114eb90df62ec8b4a1e52d8760
Dan Webb <dan.webb@damacus.io>
2020-04-21 12:24:14 +0100

    Use platform instead of platform_family
    There are two many subtle differences between Ubuntu and Debian

--- libraries/package_deps.rb
+++ libraries/package_deps.rb
@@ -13,16 +13,20 @@ class Chef
       end
 
       def cruby_package_deps
-        case node['platform_family']
+        case node['platform']
         when 'rhel', 'fedora', 'amazon'
           %w( gcc bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel make )
         when 'debian'
-          if node['platform_version'].to_i >= 18
-            %w( gcc autoconf bison build-essential libssl1.0-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev make )
-          elsif node['platform_version'].to_i >= 10
+          if node['platform_version'].to_i >= 10
             %w( gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev make )
           else
-            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev make )
+            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev make)
+          end
+        when 'ubuntu'
+          if node['platform_version'].to_i >= 18
+            %w( gcc autoconf bison build-essential libssl1.0-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev make )
+          else
+            %w(gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev make)
           end
         when 'suse'
           %w( gcc make automake gdbm-devel libyaml-devel ncurses-devel readline-devel zlib-devel libopenssl-devel )
