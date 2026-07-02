07335944f462e81987610455906fc2f2591ea67c
Dan Webb <dan.webb@damacus.io>
2026-07-02 07:10:07 +0100

    fix: avoid Supermarket during Policyfile install

--- Policyfile.rb
+++ Policyfile.rb
@@ -2,12 +2,12 @@
 
 name 'ruby_build'
 
-default_source :supermarket
-
 run_list 'test::default'
 
 cookbook 'ruby_build', path: '.'
 cookbook 'test', path: 'test/cookbooks/test'
+cookbook 'homebrew', git: 'https://github.com/sous-chefs/homebrew.git', branch: 'main'
+cookbook 'yum-epel', git: 'https://github.com/sous-chefs/yum-epel.git', branch: 'main'
 
 Dir.entries('./test/cookbooks/test/recipes').select { |f| f.end_with?('.rb') }.each do |test|
   test = test.delete_suffix('.rb')
